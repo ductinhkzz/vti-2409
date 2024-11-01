b6919d35be461390fc296eb0381563c883e0b34d
Tinh Le Duc <tinh.leduc@nashtechglobal.com>
2024-11-01 22:54:48 +0700

    Add footer

--- jquery/layouts/header.js
+++ jquery/layouts/header.js
@@ -6,20 +6,20 @@ $(document).ready(function () {
   }
 
   const collectionNav = $('#collection-nav');
+  const collectionSessionList = $('#colection-list');
   const collectionSession = $('#collection-session ul');
-  const collectionSessionContainer = $('#collection-session')
 
   $.ajax({
     url: `http://localhost:1337/api/collections?populate=*`,
     success: function (result) {
       const data = result.data;
       for (const item of data) {
-        const slug =item.slug ?? '#'
+        const slug = item.slug ?? '#';
         collectionNav.append(`
           <li class="nav-item">
             <a class="nav-link text-white" href="${slug}">${item.name}</a>
           </li>
-        `)
+        `);
 
         if (collectionSession.length > 0) {
           collectionSession.append(`
@@ -31,17 +31,18 @@ $(document).ready(function () {
                 <span class="p-2">${item.name}</span>
               </a>
             </li>
-          `)
+          `);
         }
 
-        if (collectionSessionContainer.length > 0) {
-          collectionSessionContainer.after(`
+        if (collectionSessionList.length > 0) {
+          collectionSessionList.append(`
             <section class="container my-5 py-2" id="${item.documentId}">
-              <h1 class="text-center text-white my-5 fs-3">${item.name}</h1>
             </section>
-          `)
+          `);
 
-          $(`#${item.documentId}`).load("sessions/collection.html");
+          $(`#${item.documentId}`).load('sessions/collection.html', function () {
+            $(`#${item.documentId} ul`).before(`<h1 class="text-center text-white my-5 fs-3">${item.name}</h1>`);
+          });
         }
       }
     },
--- jquery/main.js
+++ jquery/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   $("#header").load("layouts/header.html");
   $("#banner").load("sessions/banner.html");
+  $("footer").load("layouts/footer.html");
 });
